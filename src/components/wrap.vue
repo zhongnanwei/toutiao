42b2fb656a34f1aa74fbb2e0afa0bb2c8a7e5a4d
zhongnanwei <7521218+zhongnanwei@user.noreply.gitee.com>
2020-11-16 18:13:31 +0800

    完成登录页面

---
+++ src/components/wrap.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="top"></div>
+    <div class="bottom"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.top {
+  border-width: 0px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 100vw;
+  height: 24/360 * 100vw;
+  background-color: rgba(117, 117, 117, 1);
+  border-radius: 0px;
+}
+.bottom {
+  position: absolute;
+  left: 0px;
+  bottom: 0px;
+  width: 100vw;
+  height: 48/360 * 100vw;
+  background-color: rgba(0, 0, 0, 1);
+}
+</style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,18 +1,71 @@
 <template>
-  <div>
-    <Common></Common>
-    <span class="iconfont iconicon-test"></span>
+  <div class="container">
+    <div class="btnClose">
+      <span class="iconfont iconicon-test"></span>
+    </div>
+    <div class="logo">
+      <span class="iconfont iconnew"></span>
+    </div>
+    <div class="inputWrapper">
+      <input type="text" />
+    </div>
+    <div class="inputWrapper">
+      <input type="text" />
+    </div>
+    <div class="btnSubmit">
+      <button>登录</button>
+    </div>
+    <Wrap></Wrap>
   </div>
 </template>
 
 <script>
-import Common from "@/components/common.vue";
+import Wrap from "../components/wrap.vue";
 export default {
-  components: {
-    Common,
-  },
+  components: { Wrap },
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  padding: 24/360 * 100vw;
+}
+.btnClose {
+  .iconfont {
+    font-size: 28/360 * 100vw;
+  }
+}
+.logo {
+  text-align: center;
+  .iconfont {
+    font-size: 126/360 * 100vw;
+    color: #d81e06;
+  }
+}
+.inputWrapper {
+  padding-bottom: 16/360 * 100vw;
+  input {
+    width: 100%;
+    height: 48/360 * 100vw;
+    line-height: 48/360 * 100vw;
+    font-size: 18/360 * 100vw;
+    border: none;
+    outline: none;
+    border-bottom: 1px solid #333;
+  }
+}
+.btnSubmit {
+  padding-top: 26/360 * 100vw;
+  button {
+    width: 100%;
+    // height: 48/360*100vw;
+    line-height: 48/360 * 100vw;
+    font-size: 18px;
+    background: #cc3300;
+    color: white;
+    border: none;
+    outline: none;
+    border-radius: 24/360 * 100vw;
+  }
+}
 </style>
